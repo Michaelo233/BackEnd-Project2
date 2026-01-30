fc86f266155b61614b607a56a19f9486eec1d351
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-01-30 11:19:10 -0600

    Feat: Added route  for the get all tickets controller/ api get way. in the ticket routes.ts file.

---
+++ src/api/v1/routes/ticketRoutes.ts
@@ -0,0 +1,6 @@
+import express, { Router } from "express";
+import * as ticketController from "../controllers/ticketController"
+
+const ticketRouter: Router = express.Router();
+
+ticketRouter.get("/tickets", ticketController.getAllTickets);
