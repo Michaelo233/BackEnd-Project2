a409772dde5a9139777b0ce793e76f642f9b878b
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-01-30 11:04:48 -0600

    feat: Added get all tickets controller in the ticket controller.ts file.

---
+++ src/api/v1/controllers/ticketController.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { HTTP_STATUS } from "src/constants/httpConstants";
+import { Ticket } from "../models/ticketModel";
+import * as tickerService from "../services/ticketServices";
+// import { tickets } from "src/data/ticketData";
+
+export const getAllTickets = async(req: Request, res: Response, next:NextFunction): Promise<void> => {
+    try{
+        const tickets: Ticket[] = await tickerService.getAllTickets();
+
+        res.status(HTTP_STATUS.OK).json({
+            message: "Tickets retrieved",
+            count: tickets.length,
+            data: tickets,
+        });
+    } catch (error) {
+        next(error);
+    }
+}
