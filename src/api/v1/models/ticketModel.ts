e5b176138f9411d35624373b70891f6d37f73d6a
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-02-01 13:44:36 -0600

    Feat: Added updateAt in the ticket interface.

--- src/api/v1/models/ticketModel.ts
+++ src/api/v1/models/ticketModel.ts
@@ -5,6 +5,7 @@ export interface Ticket {
     priority: string;
     status: string;
     createdAt: string;
+    updatedAt?: string;
     ticketAge?: number;
     urgencyScore?: number;
     urgencyLevel?: string;
