6b4b285c24754d1005c32c11c4ca60c7b7bfb516
Michael Obikwere <oobikwere@academic.rrc.ca>
2026-02-01 11:23:18 -0600

    feat: Created get ticket urgency to get tickets urgency in service.

--- src/api/v1/services/ticketServices.ts
+++ src/api/v1/services/ticketServices.ts
@@ -1,6 +1,26 @@
 import { Ticket } from "../models/ticketModel";
-import { tickets } from "../../../data/ticketData";
+import { tickets, ticketAge, urgencyScore, urgencyLevel } from "../../../data/ticketData";
 
 export const getAllTickets = async (): Promise<Ticket[]> => {
     return structuredClone(tickets);
 } 
+
+export const getTicketUrgency = async (id: number): Promise<Ticket> => {
+    const ticket = tickets.find(ticket => ticket.id === id)
+    if(!ticket) {
+        throw new Error(`Event with ID ${id} not found`);
+    }
+    const ticketUrgency: Ticket = {
+        id: ticket.id,
+        title: ticket.title,
+        description: ticket.description,
+        priority: ticket.priority,
+        status: ticket.status,
+        createdAt: ticket.createdAt,
+        ticketAge: ticketAge(ticket.id),
+        urgencyScore: urgencyScore(ticket.id),
+        urgencyLevel: urgencyLevel(ticket.id),
+    }
+
+    return structuredClone(ticketUrgency)
+}
